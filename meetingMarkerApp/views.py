e1cd6832cd525c1ecc8a4715dbcfde34d3487af0
Marcus <marcusg@bixly.com>
2016-07-18 13:07:03 -0700

    Added meeting

--- meetingMarkerApp/views.py
+++ meetingMarkerApp/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
-
+from .models import Meeting
 # Create your views here.
 
 def index(request):
     #return HttpResponse("Arrived.")
     template = loader.get_template('meetingMarkerApp/index.html')
+    meetings = Meeting.objects.filter()
     context = {
+        "meetings": meetings
     }
     return HttpResponse(template.render(context, request))

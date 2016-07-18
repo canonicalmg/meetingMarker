94b011787e0a95e3d330e42f202b09ef03fa1bea
Marcus <marcusg@bixly.com>
2016-07-18 11:51:35 -0700

    Added meeting

--- meetingMarkerApp/models.py
+++ meetingMarkerApp/models.py
@@ -1,5 +1,16 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import datetime
 
 # Create your models here.
+
+class Meeting(models.Model):
+    users = models.ManyToManyField(User)
+    title = models.CharField(max_length=300, blank=True)
+    startTime = models.DateTimeField(default=datetime.now)
+    endTime = models.DateTimeField(default=datetime.now)
+
+    def __unicode__(self):
+        return self.title
